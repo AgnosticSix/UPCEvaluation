d676dda129426234c1b93419c5f85fef1def64ca
AgnosticSix <mateo_godfather@hotmail.com>
2016-07-05 14:19:38 -0500

    Some changes in DB classes

--- app/src/main/java/edu/upc/sw/upcevaluation/DBHandler.java
+++ app/src/main/java/edu/upc/sw/upcevaluation/DBHandler.java
@@ -13,11 +13,8 @@ public class DBHandler extends SQLiteOpenHelper{
 
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "UPCEvaluationDB.db";
-    private static final String TABLE_GRUPOS = "grupos";
 
-    public static final String COLUMN_ID = "_id";
-    public static final String COLUMN_IDASIGNATURA = "idasignatura";
-    public static final String COLUMN_DESCRIPCION = "descripcion";
+    public static final String QUERY = "";
 
     public DBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, DATABASE_NAME, factory, DATABASE_VERSION);
@@ -25,37 +22,38 @@ public class DBHandler extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String CREATE_PRODUCTS_TABLE = "CREATE TABLE " +
+        String CREATE_TABLES = QUERY;
+        /*String CREATE_PRODUCTS_TABLE = "CREATE TABLE " +
                 TABLE_GRUPOS + "("
                 + COLUMN_ID + " INTEGER PRIMARY KEY," + COLUMN_IDASIGNATURA
-                + " TEXT," + COLUMN_DESCRIPCION + " INTEGER" + ")";
-        sqLiteDatabase.execSQL(CREATE_PRODUCTS_TABLE);
+                + " TEXT," + COLUMN_DESCRIPCION + " INTEGER" + ")";*/
+        sqLiteDatabase.execSQL(CREATE_TABLES);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_GRUPOS);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS ");
         onCreate(sqLiteDatabase);
     }
 
     public void addGrupo(Grupos grupos) {
 
         ContentValues values = new ContentValues();
-        values.put(DBHandler.COLUMN_ID, "1");
+        /*values.put(DBHandler.COLUMN_ID, "1");
         values.put(DBHandler.COLUMN_IDASIGNATURA, "ingles1");
         values.put(DBHandler.COLUMN_DESCRIPCION, "Inglés 1");
         values.put(DBHandler.COLUMN_ID, "2");
         values.put(DBHandler.COLUMN_IDASIGNATURA, "algebra1");
-        values.put(DBHandler.COLUMN_DESCRIPCION, "Algebra 1");
+        values.put(DBHandler.COLUMN_DESCRIPCION, "Algebra 1");*/
 
         SQLiteDatabase db = this.getWritableDatabase();
 
-        db.insert(TABLE_GRUPOS, null, values);
+        //db.insert(TABLE_GRUPOS, null, values);
         db.close();
     }
 
     public Grupos findGrupo(String groupname) {
-        String query = "Select * FROM " + TABLE_GRUPOS + " WHERE " + COLUMN_IDASIGNATURA + " =  \"" + groupname + "\"";
+        //String query = "Select * FROM " + TABLE_GRUPOS + " WHERE " + COLUMN_IDASIGNATURA + " =  \"" + groupname + "\"";
 
         SQLiteDatabase db = this.getWritableDatabase();
 
@@ -69,7 +67,7 @@ public class DBHandler extends SQLiteOpenHelper{
             grupos.set_idAsignatura(cursor.getString(1));
             grupos.set_descripcion(cursor.getString(2));
             cursor.moveToNext();
-            Log.i(TABLE_GRUPOS,COLUMN_ID +""+ COLUMN_IDASIGNATURA +""+ COLUMN_DESCRIPCION);
+            //Log.i(TABLE_GRUPOS,COLUMN_ID +""+ COLUMN_IDASIGNATURA +""+ COLUMN_DESCRIPCION);
         }
         cursor.close();
         db.close();
