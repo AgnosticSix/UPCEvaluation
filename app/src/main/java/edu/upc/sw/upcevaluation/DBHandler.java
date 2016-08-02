a428184f1c64df347e469626ca7e31f5015888b4
AgnosticSix <mateo_godfather@hotmail.com>
2016-08-02 18:28:15 -0500

    Some changes

--- app/src/main/java/edu/upc/sw/upcevaluation/DBHandler.java
+++ app/src/main/java/edu/upc/sw/upcevaluation/DBHandler.java
@@ -29,7 +29,7 @@ public class DBHandler extends SQLiteOpenHelper{
         Log.i(TAG, "Creando instancia 2");
         for (String s: DDL.split(";")) {
             Log.i(TAG, s);
-            sqLiteDatabase.rawQuery(s,null);
+            sqLiteDatabase.execSQL(s,null);
         }
     }
 
@@ -44,58 +44,4 @@ public class DBHandler extends SQLiteOpenHelper{
         onCreate(sqLiteDatabase);*/
     }
 
-    public void addGrupo(Grupos grupos) {
-
-        ContentValues values = new ContentValues();
-
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        //db.insert(TABLE_GRUPOS, null, values);
-        db.close();
-    }
-
-    /*public Grupos findGrupo(String groupname) {
-        //String query = "Select * FROM " + TABLE_GRUPOS + " WHERE " + COLUMN_IDASIGNATURA + " =  \"" + groupname + "\"";
-
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        Cursor cursor = db.rawQuery(query, null);
-
-        Grupos grupos = new Grupos();
-
-        cursor.moveToFirst();
-        while(cursor.isAfterLast() == false) {
-            grupos.set_id(Integer.parseInt(cursor.getString(0)));
-            grupos.set_idAsignatura(cursor.getString(1));
-            grupos.set_descripcion(cursor.getString(2));
-            cursor.moveToNext();
-            //Log.i(TABLE_GRUPOS,COLUMN_ID +""+ COLUMN_IDASIGNATURA +""+ COLUMN_DESCRIPCION);
-        }
-        cursor.close();
-        db.close();
-        return grupos;
-    }*/
-
-    /*public boolean deleteGrupo(String groupname) {
-
-        boolean result = false;
-
-        String query = "Select * FROM " + TABLE_GRUPOS + " WHERE " + COLUMN_IDASIGNATURA + " =  \"" + groupname + "\"";
-
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        Cursor cursor = db.rawQuery(query, null);
-
-        Grupos grupos = new Grupos();
-
-        if (cursor.moveToFirst()) {
-            grupos.set_id(Integer.parseInt(cursor.getString(0)));
-            db.delete(TABLE_GRUPOS, COLUMN_ID + " = ?",
-                    new String[] { String.valueOf(grupos.get_id()) });
-            cursor.close();
-            result = true;
-        }
-        db.close();
-        return result;
-    }*/
 }
--- app/src/main/java/edu/upc/sw/upcevaluation/MainActivity.java
+++ app/src/main/java/edu/upc/sw/upcevaluation/MainActivity.java
@@ -70,7 +70,7 @@ public class MainActivity extends AppCompatActivity
         //rp.put("params2", value2);
 
         DummyContent.ITEMS = new ArrayList<DummyContent.DummyItem>();
-        client.get("http://192.168.1.108:81/generaJSON.php", null, new JsonHttpResponseHandler() {
+        client.get("http://192.168.1.104:81/generaJSON.php", null, new JsonHttpResponseHandler() {
 
             @Override
             public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject response) {
@@ -84,39 +84,38 @@ public class MainActivity extends AppCompatActivity
                     JSONArray rows = response.getJSONArray("Profesor");
 
                     Log.i("ITEMS", rows.length() + "");
-                    //if (rows != null && rows.length() > 0) {
+                    if (rows != null && rows.length() > 0) {
                         JSONObject item;
                         DummyContent.DummyItem obj = null;
                         SQLiteDatabase db = DBHandler.getDB(getApplicationContext());
 
 
-                        /*for (int i = 0; i < rows.length(); i++) {
+                        for (int i = 0; i < rows.length(); i++) {
                             item = rows.getJSONObject(i);
                             Log.i("ITEMS", item.getString("idProfesor"));
-                            String query = "insert into Profesor values("+item.getString("idProfesor") + ",'" + item.getString("nombre")+");"+
-                                    "insert into Cursos values("+item.getString("idCurso")+ "," + item.getString("idProfesor") + ",'" +
-                                    item.getString("descripcion")+ "','" + item.getString("hrIni")+ "','"+ item.getString("hrFin") + "';," +
+                            String query = "insert into Profesor values(" + item.getString("idProfesor") + ",'" + item.getString("nombre") + ");" +
+                                    "insert into Cursos values(" + item.getString("idCurso") + "," + item.getString("idProfesor") + ",'" +
+                                    item.getString("descripcion") + "','" + item.getString("hrIni") + "','" + item.getString("hrFin") + "';," +
                                     item.getString("idAlumno");
                             obj = new DummyContent.DummyItem(item.getString("idProfesor"), item.getString("nombre"), null);
                             DummyContent.addItem(obj);
                             DummyContent.ITEMS.add(obj);
                             db.execSQL(query);
-                            Log.i("Query: ",query);
+                            Log.i("Query: ", query);
 
-                        }*/
-                        //}
+                        }
 
-                        /*String query2 = "select * from Profesor";
-                        //db.execSQL(query2);
+
+                        String query2 = "select * from Profesor";
                         Cursor curs = db.rawQuery(query2, null);
                         curs.moveToFirst();
                         while (curs.moveToNext()) {
                             Log.i("item: ", curs.getInt(1) + "");
                         }
-                        curs.close();*/
-
+                        curs.close();
                         db.close();
-                    //}
+                    }
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
